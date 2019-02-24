7a7a925cc8d039365f4cd8410a5021d0b1f4691d
Your Name <you@example.com>
2019-02-24 12:20:33 -0800

    eieiejiejoedhgvu
    fg9eirfeofd

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,33 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
 
+	private Thread[] threads;
+	private ConcurrentLinkedQueue<Task> queue;
+	
+	public ThreadPool(int threadLength) {
+		threads = new Thread[threadLength];
+		queue = new ConcurrentLinkedQueue<Task>();
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(new Worker(queue));
+		}
+	}
+	
+	public void addTask(Task task) {
+		queue.add(task);
+	}
+	
+	public void start() {
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,12 +1,22 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
 
+	private ConcurrentLinkedQueue<Task> queue;
+	
+	public Worker(ConcurrentLinkedQueue<Task> queue) {
+		this.queue = queue;
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		while(!queue.isEmpty()) {
+			Task task = queue.remove();
+			task.perform();
+		}
 	}
 
 
